cc9ed144deec56ebdfa397b8e2a03f23f7691324
dallyh <hon.dalibor@gmail.com>
2024-04-14 22:59:22 +0200

    Error in a component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
-    output: "hybrid",
+    output: "server",
     adapter: cloudflare({
         imageService: "passthrough",
         platformProxy: {
             enabled: true,
         },
     }),
-    integrations: [react()],
 });
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,5 @@
 import type { APIRoute } from "astro";
 
-export const prerender = false;
-
 export const GET: APIRoute = ({ preferredLocale, redirect, cookies }) => {
     return redirect("/test");
 };
