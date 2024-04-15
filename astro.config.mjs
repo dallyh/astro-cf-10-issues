c4eb8b432aa2493f9b4e8c490aa19deea08e8e0f
Dalibor Hon <dalibor.hon@webcom.cz>
2024-04-15 08:03:07 +0200

    Add Node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
+
+const cloudflareAdapter = cloudflare({
+    imageService: "passthrough",
+    platformProxy: {
+        enabled: true,
+    },
+});
+
+const nodeAdapter = node({
+    mode: "standalone",
+});
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: cloudflare({
-        imageService: "passthrough",
-        platformProxy: {
-            enabled: true,
-        },
-    }),
+    adapter: cloudflareAdapter,
 });
