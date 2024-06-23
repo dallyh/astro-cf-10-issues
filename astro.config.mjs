03d08ccb40dfe41a21438321e43451b59bf4adbf
dallyh <hon.dalibor@gmail.com>
2024-06-23 16:32:35 +0200

    Update deps and simplify reproduction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
     output: "hybrid",
@@ -11,9 +9,5 @@ export default defineConfig({
         platformProxy: {
             enabled: true,
         },
-        experimental: {
-            manualChunks: ["sharp"]
-        }
     }),
-    integrations: [react()],
 });
